a27bfda410223754b0fb0b2e83db103c56441780
XB19 <josuekomadan@gmail.com>
2025-12-08 20:53:37 +0000

    Historique des évènements

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/events_screen.dart';
+import 'app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const EventsScreen(),
-    );
-  }
 }
